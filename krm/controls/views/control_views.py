4a4fc9108714bbd733adca47cc83ec23029b9be3
symrzknr <mrevuelta.deca@gmail.com>
2022-10-20 13:49:15 +0200

    Prefetch Control a risk-risk_master-domain_risk

--- krm/controls/views/control_views.py
+++ krm/controls/views/control_views.py
@@ -42,7 +42,7 @@ class GaControlListView(ListView):
     template_name = 'controls/GaControlList.html'
     context_object_name = 'controls'
     queryset = Control.objects.all().prefetch_related(
-        'sub_processes').prefetch_related('risks')
+        'sub_processes').prefetch_related('risks__risk_master__domain_risk')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
