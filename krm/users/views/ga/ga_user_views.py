9bbe1f80a8cc153efed230b142ffbd8717f9967f
symrzknr <mrevuelta.deca@gmail.com>
2022-10-27 15:43:27 +0200

    FAKEBOARD - atrocidades
    (lo que hay en master.html no he conseguido que vaya en la propia template GaDashboard.html) -zorry

--- krm/users/views/ga/ga_user_views.py
+++ krm/users/views/ga/ga_user_views.py
@@ -7,7 +7,7 @@ from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_protect
 
 from django.contrib import messages
-
+import pandas as pd
 
 from django.views.generic import (
     FormView,
@@ -48,4 +48,12 @@ class GaDashboardView(TemplateView):
         context['page_title'] = _(
             'Dashboard para el Administrador Global')
         context['breadcrums'] = breadcrums
+        df = pd.DataFrame(
+            [{'tipo': 'Autopista', 'info': 'Third-party screening and due diligence', 'ref': 'USA-R07', 'x': 4.0, 'y': 3.0}, {'tipo': 'Autopista', 'info': 'Liability arising from employees ', 'ref': 'USA-R05', 'x': 3.0, 'y': 3.0}, {'tipo': 'Autopista', 'info': 'Financiación ilegal de partidos políticos', 'ref': 'GRU-R11', 'x': 3.2, 'y': 2.0}, {'tipo': 'Autopista', 'info': 'Malversación', 'ref': 'GRU-R28', 'x': 2.7, 'y': 2.3}, {'tipo': 'Autopista', 'info': 'Corrupción en los negocios', 'ref': 'GRU-R02', 'x': 3.1, 'y': 1.9}, {'tipo': 'Corporación', 'info': 'Cohecho', 'ref': 'GRU-R01', 'x': 4.0, 'y': 3.0}, {'tipo': 'Corporación', 'info': 'Tráfico de Influencias', 'ref': 'GRU-R03', 'x': 4.0, 'y': 3.0}, {'tipo': 'Corporación', 'info': 'Estafa', 'ref': 'GRU-R05', 'x': 4.0, 'y': 3.0}, {'tipo': 'Corporación', 'info': 'Malversación', 'ref': 'GRU-R28', 'x': 4.0, 'y': 3.0}, {'tipo': 'Corporación', 'info': 'Mercado y Consumidores', 'ref': 'GRU-R07', 'x': 4.0, 'y': 2.0}, {'tipo': 'Corporación', 'info': 'Financiación ilegal de partidos políticos', 'ref': 'GRU-R11', 'x': 4.0, 'y': 2.0}, {'tipo': 'Corporación', 'info': 'Corrupción en los negocios', 'ref': 'GRU-R02', 'x': 2.5, 'y': 2.5}, {'tipo': 'Corporación', 'info': 'Descubrimiento y revelación de secretos', 'ref': 'GRU-R04', 'x': 2.0, 'y': 3.0}, {'tipo': 'Ferrocarril', 'info': 'Falsificación de tarjetas de crédito, débito y cheques de viaje', 'ref': 'GRU-R14', 'x': 4.0, 'y': 4.0}, {'tipo': 'Ferrocarril', 'info': 'Cohecho', 'ref': 'GRU-R01', 'x': 4.0, 'y': 3.0}, {'tipo': 'Ferrocarril', 'info': 'Tráfico de Influencias', 'ref': 'GRU-R03', 'x': 4.0, 'y': 3.0}, {'tipo': 'Ferrocarril', 'info': 'Financiación ilegal de partidos políticos', 'ref': 'GRU-R11', 'x': 4.0, 'y': 3.0}, {'tipo': 'Ferrocarril', 'info': 'Malversación', 'ref': 'GRU-R28', 'x': 4.0, 'y': 3.0}, {'tipo': 'Innovación', 'info': 'Propiedad Industrial e Intelectual', 'ref': 'GRU-R18', 'x': 3.7, 'y': 3.0}, {'tipo': 'Innovación', 'info': 'Corrupción en los negocios', 'ref': 'GRU-R02', 'x': 3.8, 'y': 2.5}, {'tipo': 'Innovación', 'info': 'Liability arising from employees ', 'ref': 'USA-R05', 'x': 3.0, 'y': 3.0}, {'tipo': 'Innovación', 'info': 'Descubrimiento y revelación de secretos', 'ref': 'GRU-R04', 'x': 3.4, 'y': 2.6}, {'tipo': 'Innovación', 'info': 'Blanqueo de capitales', 'ref': 'GRU-R10', 'x': 3.8, 'y': 2.2}]
+        )
+        df['severidad'] = df['x']*df['y']
+        df = df.sort_values('severidad', ascending=False)
+        context['df'] = df
+        context['js_template'] = ['js/custom/datatables.js']
+        
         return context
