38866e903a85af1a690d8031f688d477c25ab83f
symrzknr <mrevuelta.deca@gmail.com>
2022-10-30 19:20:32 +0100

    Nueva pantalla RuInherentDetail - ru_test_inherent_view
    cuidados @user
    - reverse url de breadcrumbs a GA, sustituida RU
    - get_success_url a GA, sustituida a RU
    - filtrados los RiskInherentTest obtenidos para user = self.request.user

--- krm/evaluations_krm/views/ru_risk_test_inherent_views.py
+++ krm/evaluations_krm/views/ru_risk_test_inherent_views.py
@@ -1,5 +1,5 @@
 
-
+import json
 from django.shortcuts import render
 
 # Create your views here.
@@ -19,12 +19,14 @@ from django.contrib import messages
 from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse_lazy, reverse
 from django.utils.translation import gettext as _
+from django.forms.models import model_to_dict
 
 from django.shortcuts import get_object_or_404
 
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from krm.evaluations_krm.models.risk_test_inherent_model import RiskTestInherent
+from krm.evaluations_krm.models import EvaluationKrmInherent
 
 from krm.metronic.__init__ import KTLayout
 from krm.metronic.libs.theme import KTTheme
@@ -35,6 +37,10 @@ from krm.evaluations_krm.models import (
 
 from krm.evaluations_krm.forms import (
     EvaluationInherenetCompleteForm,
+)
+
+from krm.evaluations.forms.evaluation_forms import (
+    EvaluationActionForm, 
 )
 
 
@@ -142,4 +148,114 @@ class RuEvaluationRiskInherentComplete(DetailView, FormView):
 
         return reverse_lazy(
             "evaluations_krm:ru_evaluation_risk_inherent_list"
+        )
+
+
+@method_decorator((login_required, ), name="dispatch")
+class RuEvaluationRiskInherentDetail(FormView):
+    template_name = 'evaluations_krm/RuEvaluationInherentDetail.html'
+    form_class = EvaluationActionForm
+
+    def dispatch(self, request, *args, **kwargs):
+        evaluation_krm = get_object_or_404(
+            EvaluationKrmInherent, pk=self.kwargs.get("pk"))
+        self.evaluation = evaluation_krm
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context = KTLayout.init(context)
+        context['evaluation'] = self.evaluation
+        breadcrums = [
+            {'title': _('Dashboard'), 'url': reverse('users:dashboard')},
+            {'title': _('Evaluaciones KRM'), 'url': reverse(
+                'evaluations_krm:ru_evaluation_risk_inherent_list')},
+            {'title': self.evaluation.ref}
+        ]
+        context['page_title'] = f"{_('Evaluación KRM Inherent')} : {self.evaluation.ref}"
+        context['breadcrums'] = breadcrums
+        
+        context['evaluation'].nrisk_test_inherents_pending = context['evaluation'].nrisk_test_inherents_by_state(1, user=self.request.user)
+        context['evaluation'].nrisk_test_inherents_delivered = context['evaluation'].nrisk_test_inherents_by_state(2, user=self.request.user)
+        context['evaluation'].nrisk_test_inherents_finished = context['evaluation'].nrisk_test_inherents_by_state(3, user=self.request.user)
+        
+        # Serializar Evaluation no incluye sus hijos :(
+        # Busco los hijos
+        context['rit'] = RiskTestInherent.objects.filter(
+            evaluation = self.evaluation,
+            expert = self.request.user,
+            )
+        
+        # Paso a dict para json
+        context['rit_dict'] = [model_to_dict(m) for m in context['rit']]
+        
+        # MODEL_TO_DICT not getting properties :(
+        # Get .severity_level_expert
+        # TBI for cuadratico :/
+        # Los risk_inherent_test no tienen ref ni name, es heredado del risk_company
+        for i,r1 in enumerate(context['rit']):
+            context['rit_dict'][i]['risk_ref'] = r1.risk.risk.ref
+            context['rit_dict'][i]['risk_name'] = r1.risk.risk.name
+            for r2 in context['rit_dict']:
+                if r1.id == r2['id']: r2['severity_level_expert'] = r1.severity_level_expert
+
+        # Sort by severity for a nice plot
+        context['rit_dict'] = sorted(context['rit_dict'], key= lambda x: (x['severity_level_expert'], x['risk_ref']), reverse = True)
+
+        # Errores de encoding caracteres portugueses y españoles
+        for i,m in enumerate(context['rit_dict']):
+            for k in m:
+                if type(context['rit_dict'][i][k]) == str:
+                    context['rit_dict'][i][k] = context['rit_dict'][i][k].encode('utf-8').decode('utf-8')
+
+        # JSON DUMP
+        context['rit_json'] = json.dumps(
+            context['rit_dict'],
+            default=str,
+            ensure_ascii=True,
+            )
+        
+        context['js_template'] = ['js/custom/datatables.js']
+
+        return context
+
+    def form_valid(self, form):
+        action = form.cleaned_data["action"]
+        evaluation = self.evaluation
+
+        # if action == "i":
+        #     evaluation.status = "EP"
+        #     evaluation.save()
+        #     users_notificated = []
+        #     for ct in evaluation.control_tests.all():
+        #         ct.status = "WO"
+        #         ct.save()
+        #         if ct.control_test_owner not in users_notificated:
+        #             users_notificated.append(ct.control_test_owner)
+        #             ct.send_notification()
+
+        #     messages.add_message(
+        #         self.request,
+        #         messages.SUCCESS,
+        #         _("Evaluación iniciada correctamente"),
+        #     )
+        # elif action == 'f':
+        #     evaluation.status = "FI"
+        #     evaluation.save()
+        #     evaluation.control_tests.update(
+        #         status='FI'
+        #     )
+
+        #     messages.add_message(
+        #         self.request,
+        #         messages.SUCCESS,
+        #         _("Evaluación finalizada correctamente"),
+        #     )
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy(
+            "evaluations_krm:ru_evaluation_krm_inherent_detail",
+            kwargs={"pk": self.evaluation.pk},
         )
