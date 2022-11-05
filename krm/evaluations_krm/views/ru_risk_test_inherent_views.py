6f829028d6cea1408b3f12323cca1c526f5ff533
symrzknr <mrevuelta.deca@gmail.com>
2022-11-05 17:55:54 +0100

    Errata

--- krm/evaluations_krm/views/ru_risk_test_inherent_views.py
+++ krm/evaluations_krm/views/ru_risk_test_inherent_views.py
@@ -117,7 +117,7 @@ class RuEvaluationRiskInherentComplete(DetailView, FormView):
             {'title': _('Dashboard'), 'url': reverse('users:dashboard')},
             {'title': _('Evaluaciones de Riesgo Inherente')}
         ]
-        context['page_title'] = f"{_('Evaluación de Riesgos Inherentes')} : {self.object.ref}"
+        context['page_title'] = f"{_('Evaluación de Riesgo Inherente')} : {self.object.ref}"
         context['breadcrums'] = breadcrums
 
         risk_tests = self.object.risk_test_inherents.filter(
