eb17619bb28a9e7c3ed8232320069fc44eb78753
symrzknr <mrevuelta.deca@gmail.com>
2022-11-27 16:15:46 +0100

    Ga/Ca/Ru/EvaluationInherentDetail - views
    Quitar de los datos json para el gráfico de barras datos que no se necesitan (muchos textos de los evaluadores dan errores de encoding)

--- krm/evaluations_krm/views/ca_evaluation_inherent_views.py
+++ krm/evaluations_krm/views/ca_evaluation_inherent_views.py
@@ -301,6 +301,12 @@ class CaEvaluationInherentDetailView(FormView):
                 if r1.id == r2['id']:
                     r2['severity_level_expert'] = r1.severity_level_expert
 
+        # QUITAR RESTO DE ATTRIBUTES (solo dan problemas con el encoding)
+        for i, r1 in enumerate(context['rit']):
+            for k in context['rit_dict'][i].copy():
+                if k not in ['risk_ref', 'risk_name', 'expert', 'impact_level_expert', 'probability_level_expert', 'severity_level_expert']:
+                    del context['rit_dict'][i][k]
+
         # Sort by severity for a nice plot
         context['rit_dict'] = sorted(context['rit_dict'], key=lambda x: (
             x['severity_level_expert'], x['risk_ref']), reverse=True)
