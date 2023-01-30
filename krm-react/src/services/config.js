882a858e8ef7f2e434dfe815db6ba6e2fda4844f
Bienvenido Sáez Muelas <bienvenidosaez@gmail.com>
2023-01-30 16:33:10 +0100

    Más settings de test

--- krm-react/src/services/config.js
+++ krm-react/src/services/config.js
@@ -1,10 +1,12 @@
 let base = "";
 if (window.location.hostname.indexOf("localhost") > -1) {
-    base = "http://localhost:8000";
+  base = "http://localhost:8000";
 } else if (window.location.hostname.indexOf("gvi.compliancetool.net") > -1) {
-    base = "https://gvi.compliancetool.net";
+  base = "https://gvi.compliancetool.net";
+} else if (window.location.hostname.indexOf("test.compliancetool.net") > -1) {
+  base = "https://test.compliancetool.net";
 } else {
-    base = "https://gvi.compliancetool.net";
+  base = "https://gvi.compliancetool.net";
 }
 
 const baseUrlApi = `${base}/api/`;
@@ -12,21 +14,21 @@ const baseUrlApi = `${base}/api/`;
 const user = window.$('#pk').data('pk');
 
 const configService = {
-    apiGetProcesses: `${baseUrlApi}process/`,
-    apiGetRisks: `${baseUrlApi}risks/`,
-    apiGetDomainRisks: `${baseUrlApi}domain-risks/`,
-    apiGetControls: `${baseUrlApi}controls/`,
-    apiGetCompanies: `${baseUrlApi}companies?user=${user}`,
-    apiGetRiskCompany: `${baseUrlApi}riskscompany/`,
-    apiGetRiskCompanyResidual: `${baseUrlApi}riskscompanyresidual/`,
-    apiSendRiskTestInherent: `${baseUrlApi}risktestinherentexpert/`,
-    apiSendRiskTestResidual: `${baseUrlApi}risktestresidualevaluator/`,
-    apiSendRiskCompanyResidualAdmin: `${baseUrlApi}riskcompanyresidualadmin/`,
-    apiGetControlCompany: `${baseUrlApi}controlscompany/`,
-    apiGetQuestionnaires: `${baseUrlApi}questionnaires/`,
-    apiGetQuestions: `${baseUrlApi}questions/`,
-    apiGetUsers: `${baseUrlApi}users/`,
-    apiSendQuestionTest: `${baseUrlApi}questiontest/`,
+  apiGetProcesses: `${baseUrlApi}process/`,
+  apiGetRisks: `${baseUrlApi}risks/`,
+  apiGetDomainRisks: `${baseUrlApi}domain-risks/`,
+  apiGetControls: `${baseUrlApi}controls/`,
+  apiGetCompanies: `${baseUrlApi}companies?user=${user}`,
+  apiGetRiskCompany: `${baseUrlApi}riskscompany/`,
+  apiGetRiskCompanyResidual: `${baseUrlApi}riskscompanyresidual/`,
+  apiSendRiskTestInherent: `${baseUrlApi}risktestinherentexpert/`,
+  apiSendRiskTestResidual: `${baseUrlApi}risktestresidualevaluator/`,
+  apiSendRiskCompanyResidualAdmin: `${baseUrlApi}riskcompanyresidualadmin/`,
+  apiGetControlCompany: `${baseUrlApi}controlscompany/`,
+  apiGetQuestionnaires: `${baseUrlApi}questionnaires/`,
+  apiGetQuestions: `${baseUrlApi}questions/`,
+  apiGetUsers: `${baseUrlApi}users/`,
+  apiSendQuestionTest: `${baseUrlApi}questiontest/`,
 
 };
 
