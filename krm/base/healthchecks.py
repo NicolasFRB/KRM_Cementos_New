3b5179d0a24bb0d704ea417dadd71ec0f7a7a710
Rodriguez Bon <nicolasfernandorodri@kpmg.es>
2024-11-18 10:59:11 +0100

    testing json response

--- krm/base/healthchecks.py
+++ krm/base/healthchecks.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def simpleHealthCheck(request, *callback_args, **callback_kwargs):
-    return HttpResponse('ok')
+    return JsonResponse({'status':'ok'})
