a206f9f42c4773800f6429e6da567a92da138d6d
Rodriguez Bon <nicolasfernandorodri@kpmg.es>
2024-11-27 11:14:30 +0100

    testing wso login options

--- config/settings/base.py
+++ config/settings/base.py
@@ -521,8 +521,8 @@ SAML2_AUTH = {
     'METADATA_LOCAL_FILE_PATH': 'dev-metadata.xml',
 
     # Optional settings below
-    'DEFAULT_NEXT_URL': 'en/dashboard',  # Custom target redirect URL after the user get logged in. Default to /admin if not set. This setting will be overwritten if you have parameter ?next= specificed in the login URL.
-    'CREATE_USER': 'TRUE', # Create a new Django user when a new user logs in. Defaults to True.
+    'DEFAULT_NEXT_URL': '/en/auth/callback/',  # Custom target redirect URL after the user get logged in. Default to /admin if not set. This setting will be overwritten if you have parameter ?next= specificed in the login URL.
+    'CREATE_USER': 'FALSE', # Create a new Django user when a new user logs in. Defaults to True.
     'NEW_USER_PROFILE': {
         'USER_GROUPS': [],  # The default group name when a new user logs in
         'ACTIVE_STATUS': True,  # The default active status for new users
