57ac0a80ceb806bf400678153e288a39c07e4582
Nicolas Fernando Rodriguez Bon <nicolasfernandorodri@kpmg.es>
2025-03-04 18:27:02 +0100

    added script to build react and add the files to the static folder in 1 command

---
+++ buildReact.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+cd krm/static/; 
+rm main.react.js;
+rm main.react.js.map ;
+cd ../..;
+
+cd krm-react 
+npm run build; 
+mv build/static/js/main.*.js ../krm/static/main.react.js;
+mv build/static/js/main.*.js.map ../krm/static/main.react.js.map;
+cd ..;
