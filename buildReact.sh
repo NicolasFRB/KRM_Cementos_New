09edb87074c5bd92deaf6b9d93329b9680674569
Nicolás Fernando Rodríguez Bon <nicolasfernandorodri@kpmg.es>
2025-06-06 10:33:41 +0200

    added env variable to react and added react build to start process

--- buildReact.sh
+++ buildReact.sh
@@ -6,7 +6,8 @@ rm main.react.js;
 rm main.react.js.map ;
 cd ../..;
 
-cd krm-react 
+cd krm-react;
+npm i;
 npm run build; 
 mv build/static/js/main.*.js ../krm/static/main.react.js;
 mv build/static/js/main.*.js.map ../krm/static/main.react.js.map;
